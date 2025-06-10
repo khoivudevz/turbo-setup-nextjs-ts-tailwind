f4fcb43c3cf9d008c295fec8de4f3f498a5ff96f
khoivu <khoi.vu@nextchain.kr>
2025-06-10 10:44:36 +0700

    feat: modals providers

---
+++ src/views/HomeView/components/ModalButton.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import {MODAL_KEYS} from '@/constants/modals.constant'
+import useClearModals from '@/hooks/useClearModals'
+import useModalStore from '@/stores/useModal.store'
+
+const ModalButton = () => {
+	const {openModal} = useModalStore()
+	useClearModals([MODAL_KEYS.DEMO_MODAL])
+
+	return (
+		<button
+			className='w-fit bg-transparent border-[1px] border-solid border-white text-white px-4 py-2 rounded-md my-[30px] hover:bg-white hover:text-black transition-all duration-300'
+			onClick={() => openModal(MODAL_KEYS.DEMO_MODAL)}
+		>
+			Open Modal
+		</button>
+	)
+}
+
+export default ModalButton
