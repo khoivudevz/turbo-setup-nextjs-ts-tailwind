0f378e2df8dc63f9d81ec90efb2cfca8a85ebf2a
Khoi Vu <khoi.vu@nextchain.kr>
2026-07-07 13:41:02 +0700

    feat: update packages

--- src/services/dumb.service.ts
+++ src/services/dumb.service.ts
@@ -3,7 +3,7 @@ import axios, {AxiosResponse} from 'axios'
 const dumbServices = {
 	async getNews(params: any): Promise<AxiosResponse<any>> {
 		return axios({
-			url: 'https://dumbstockapi.com/stock?exchanges=NYSE',
+			url: 'https://api.restful-api.dev/objects',
 			method: 'GET',
 			params: params,
 		})
